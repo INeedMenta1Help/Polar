6100dfe004b2a720690c6b365b5f23be825f449e
INeedMenta1Help <32667276+INeedMenta1Help@users.noreply.github.com>
2018-11-14 13:26:52 +0000

    Basic Variable Declaration defined
    For syntax and small demo, check test.plr in any text editor.
    Created With Python: https://www.python.org/
    Edited With Sublime: https://www.sublimetext.com/

---
+++ main.py
@@ -0,0 +1,87 @@
+from sys import *
+file = list(open(argv[1], "r").read())
+
+#Arrays
+memSize = 64
+mem = [None] * memSize
+var = []
+
+def findVar(name):
+	for i in range(0, len(var)):
+		if name == var[i][0]:
+			return var[i][1]
+	print(name + " Not Defined - 0 Instead Of Exception!")
+	return 0
+
+def lex():
+	toks = []
+	tok = ""
+	string = False
+
+	for char in file:
+		print(tok)
+		if (char == " " or char == "\n" or char == ";") and string == False:
+			toks.append(tok)
+			tok = ""
+		elif char == "\"":
+			tok += char
+			string = not string
+		else:
+			tok += char
+	print(toks)
+
+	lex2(toks)
+
+def lex2(toks):
+	adtoks = [] #Advanced Tokens
+	for i in range(0, len(toks)):
+		if toks[i][0] == "&":
+			varname = toks[i][1:]
+			adtoks.append(["var", varname])
+		elif toks[i][0] == "$":
+			adtoks.append(["data", toks[i][1:]])
+
+		elif toks[i][0] == "\"":
+			adtoks.append(["string", toks[i][1:len(toks[i])-1]])
+
+		elif toks[i] == "=":
+			adtoks.append(["declare", "="])
+
+		elif toks[i] == "==":
+			adtoks.append(["op", "=="])
+
+		elif toks[i] == "log:":
+			adtoks.append(["func", "log"])
+
+
+	print(adtoks)
+	parse(adtoks)
+
+def parse(toks):
+	i = 0
+	while i < len(toks):
+		print(toks[i])
+
+		if toks[i][0] == "var":
+			if toks[i+1][0] == "declare":
+				if toks[i+2][0] == "data":
+					var.append([toks[i][1], int(toks[i+2][1])])
+					print("Var: " + toks[i][1] + " Created As: " + toks[i+2][1])
+				elif toks[i+2][0] == "string":
+					var.append([toks[i][1], str(toks[i+2][1])])
+				elif toks[i+2][0] == "var":
+					vardata = findVar(toks[i+2][1])
+					var.append([toks[i][1], vardata])
+				else:
+					print("Invalid Declaration Of Variable, Cannot Declare As Type: " + toks[i+2][0] + "\nVar Created With 0 To Avoid Exception")
+					var.append([toks[i][1], 0])
+				i += 2
+
+		i+=1
+
+lex()
+
+
+#print(file)
+print(mem)
+print(var)
