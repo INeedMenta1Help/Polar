4b94630e01e7d5eb3f9e176cb60226278c1035bb
INeedMenta1Help <32667276+INeedMenta1Help@users.noreply.github.com>
2018-11-14 13:53:08 +0000

    Added "Comments" and A "Log:" Function
    "Comments": Self Explanatory,
    "Log:":  A Function For Printing An Item To The Screen

--- main.py
+++ main.py
@@ -17,18 +17,25 @@ def lex():
 	toks = []
 	tok = ""
 	string = False
+	comstate = False
 
 	for char in file:
-		print(tok)
-		if (char == " " or char == "\n" or char == ";") and string == False:
-			toks.append(tok)
+		#print(tok)
+		if ((char == " " and comstate == False) or char == "\n" or char == ";") and string == False:
+			if (comstate == False and tok != ""):
+				toks.append(tok)
 			tok = ""
-		elif char == "\"":
-			tok += char
-			string = not string
-		else:
-			tok += char
-	print(toks)
+			comstate = False
+		elif char == "#":
+			comstate = True
+			tok = ""
+		elif comstate == False:
+			if char == "\"":
+				tok += char
+				string = not string
+			else:
+				tok += char
+	#print(toks)
 
 	lex2(toks)
 
@@ -38,6 +45,7 @@ def lex2(toks):
 		if toks[i][0] == "&":
 			varname = toks[i][1:]
 			adtoks.append(["var", varname])
+
 		elif toks[i][0] == "$":
 			adtoks.append(["data", toks[i][1:]])
 
@@ -54,28 +62,38 @@ def lex2(toks):
 			adtoks.append(["func", "log"])
 
 
-	print(adtoks)
+	#print(adtoks)
 	parse(adtoks)
 
 def parse(toks):
 	i = 0
 	while i < len(toks):
-		print(toks[i])
+		#print(toks[i])
 
 		if toks[i][0] == "var":
 			if toks[i+1][0] == "declare":
 				if toks[i+2][0] == "data":
 					var.append([toks[i][1], int(toks[i+2][1])])
-					print("Var: " + toks[i][1] + " Created As: " + toks[i+2][1])
+					#print("Var: " + toks[i][1] + " Created As: " + toks[i+2][1])
 				elif toks[i+2][0] == "string":
 					var.append([toks[i][1], str(toks[i+2][1])])
+					#print("Var: " + toks[i][1] + " Created As: " + str(toks[i+2][1]))
 				elif toks[i+2][0] == "var":
 					vardata = findVar(toks[i+2][1])
 					var.append([toks[i][1], vardata])
+					#print("Var: " + toks[i][1] + " Created As: " + str(vardata))
 				else:
 					print("Invalid Declaration Of Variable, Cannot Declare As Type: " + toks[i+2][0] + "\nVar Created With 0 To Avoid Exception")
 					var.append([toks[i][1], 0])
 				i += 2
+
+		elif toks[i][0] == "func":
+			if toks[i][1] == "log":
+				if toks[i+1][0] == "var":
+					print(findVar(toks[i+1][1]))
+				elif toks[i+1][0] == "string" or toks[i+1][0] == "data":
+					print(toks[i+1][1])
+				i += 1
 
 		i+=1
 
@@ -83,5 +101,5 @@ lex()
 
 
 #print(file)
-print(mem)
-print(var)
+#print(mem)
+#print(var)
